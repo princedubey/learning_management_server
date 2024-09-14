edcf9f7fe135fa0041e639572d51b2564a0ade1b
PrinceDubey <princedubey1998@gmail.com>
2024-09-14 11:47:36 +0530

    update: cookie security update

--- src/middlewares/authHandler.js
+++ src/middlewares/authHandler.js
@@ -47,21 +47,21 @@ export const setTokensInCookies = (res, accessToken, refreshToken) => {
 
   res.cookie('access_token', accessToken, {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'PROD',
     maxAge: oneHour,
     sameSite: 'Strict'
   });
 
   res.cookie('refresh_token', refreshToken, {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'PROD',
     maxAge: sevenDays,
     sameSite: 'Strict'
   });
 
   res.cookie('is_auth', true, {
     httpOnly: false,
-    secure: false,
+    secure: process.env.NODE_ENV === 'PROD',
     maxAge: sevenDays,
   });
 };
